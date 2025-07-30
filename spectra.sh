1dec9310370d5f16a58b7c3568fc1038e15d7edf
Steve <idowusteve@ymail.com>
2025-07-30 14:17:02 -0400

    Updated spectra.sh file

--- spectra.sh
+++ spectra.sh
@@ -2,6 +2,7 @@
 
 # Check for at least 2 arguments
 if [ "$#" -lt 2 ]; then
+    echo "The input directory and output directory must be specified."
     echo "Usage: spectra <input_dir> <output_dir> [optional args...]"
     exit 1
 fi
@@ -14,7 +15,4 @@ shift 2  # Remove first two args, leaving optional flags
 docker run --rm \
     -v "$INPUT_DIR":/input \
     -v "$OUTPUT_DIR":/output \
-    pangw1n/spectra --input-dir=/input --output-dir=/output "$@"
-
-
-
+    pangw1n/spectra --input=/input --output=/output "$@"
